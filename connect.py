0ab69ac7c5905ad8f0ea65080bef71d303d09357
Nakajima, Suzuko <suzukoichiharanakajima@gmail.com>
2021-01-15 19:13:19 +0000

    Update v2021.1.16.1

--- connect.py
+++ connect.py
@@ -7,14 +7,14 @@ import time
 import datetime
 
 class Connect:
-  def __init__(self, main):
-    self.main = main
+    def __init__(self, main):
+        self.main = main
 
-  print('-!- ==================== -!-')
-  print('- + - Program initialized - + -')
-  print('-!- ==================== -!-\n\n')
+    print('-!- ==================== -!-')
+    print('- + - Program initialized - + -')
+    print('-!- ==================== -!-\n\n')
 
-  print('The program is ready, type \'./help\' for a list of available commands after the setup.\n')
+    print('The program is ready, type \'./help\' for a list of available commands after the setup.\n')
 
 def setup(main):
-  main.add_cog(Connect(main))
+    main.add_cog(Connect(main))
